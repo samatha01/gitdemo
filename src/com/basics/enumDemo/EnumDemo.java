ab867a93f23231bd5ba19501704003a02c93c517
Samatha <kumarisamatha@gmail.com>
2022-02-01 18:18:42 +0530

    Updated

--- src/com/basics/enumDemo/EnumDemo.java
+++ src/com/basics/enumDemo/EnumDemo.java
@@ -3,7 +3,10 @@ package com.basics.enumDemo;
 public class EnumDemo {
 
 	public static void main(String[] args) {
-		DaysOfTheWeek day;
+		DaysOfTheWeek day=DaysOfTheWeek.FRIDAY;
+		if(day==DaysOfTheWeek.FRIDAY) {
+			System.out.println("Its almost Saturday");
+		}
 		for(DaysOfTheWeek myDay: DaysOfTheWeek.values()) {
 			System.out.println(myDay);
 		}
