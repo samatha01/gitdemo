22c39a81ee27704c641e32dff5fd4e2b3d2267d4
Samatha <kumarisamatha@gmail.com>
2022-02-03 19:08:29 +0530

    Updated

---
+++ src/com/basics/generics/DualGenericHolder.java
@@ -0,0 +1,20 @@
+package com.basics.generics;
+
+public class DualGenericHolder <T,U>{
+	T obj1;
+	U obj2;
+	DualGenericHolder(T obj1, U obj2){
+		this.obj1=obj1;
+		this.obj2=obj2;
+	}
+	public void display() {
+		System.out.println(obj1);
+		System.out.println(obj2);
+	}
+
+	public static void main(String[] args) {
+		DualGenericHolder <String, Integer> obj=new DualGenericHolder <String, Integer> ("Hello", 10);
+		obj.display();
+	}
+
+}
